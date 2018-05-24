634d53111d99f52152c8770d9e0d99ddf9683319
xlk <1148038404@qq.com>
2018-05-24 20:40:22 +0800

    slide demo

---
+++ main.js
@@ -0,0 +1,45 @@
+let n = 1;
+初始化();
+setInterval(() =>{
+    makeLeave(getImage(n))
+    .one('transitionend',(e)=>{
+        makeEnter($(e.currentTarget))
+    })
+    makeCurrent(getImage(n+1))
+    n+=1
+},3000)
+
+
+
+
+
+
+function x(n){
+    if(n>3){
+        n=n%3;
+        if(n===0){
+            n=3
+        }
+    }
+    return n;
+}
+
+function 初始化(){
+    n=1;
+    $(`.images>img:nth-child(${n})`).addClass("current")
+    .siblings().addClass('enter')
+}
+
+function getImage(n){
+    return $(`.images>img:nth-child(${x(n)})`)
+}
+
+function makeCurrent($node){
+    return $node.removeClass("leave enter").addClass("current")
+}
+function makeLeave($node){
+    return $node.removeClass("current enter").addClass("leave")
+}
+function makeEnter($node){
+    return $node.removeClass("current leave").addClass("enter")
+}
